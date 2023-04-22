2c95ac25aa2b04476b9d1c9eec09b239fca6f61b
bumpyclock <adityaksharma@gmail.com>
2023-04-22 09:07:39 -0700

    added cors proxy

---
+++ background_fetchFeeds_src.js
@@ -0,0 +1,21 @@
+const RSSParser = require('rss-parser');
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'fetchFeed') {
+        console.log('fetching feed', request.feedURL)
+      fetchFeed(request.feedURL)
+        .then(feed => sendResponse({ success: true, feed }))
+        .catch(error => sendResponse({ success: false, error }));
+      return true;
+    }
+  });
+  
+  async function fetchFeed(feedURL) {
+    const parser = new RSSParser();
+    const corsProxy = 'https://figma-plugin-cors-proxy.azurewebsites.net/proxy?url=';
+    const proxiedURL = corsProxy + encodeURIComponent(feedURL);
+    const feed = await parser.parseURL(proxiedURL);
+    return feed;
+  }
+  
+  
--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const SUBSCRIBED_FEEDS_KEY = 'subscribedFeeds';
-const DEFAULT_FEED_URL = 'https://www.theverge.com/rss/index.xml';
+const DEFAULT_FEED_URL = 'https://daringfireball.net/feeds/main';
 
 document.addEventListener('DOMContentLoaded', () => {
   const date = new Date();
@@ -15,23 +15,116 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   document.title = `${greeting} - RSS Feed Reader`;
-  document.getElementById('greeting').textContent = greeting;
-  loadSubscribedFeeds();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('#feed-container')) {
     // Main page
+    document.getElementById('greeting').textContent = greeting;
+
     loadSubscribedFeeds();
+    
   } else {
     // Settings page
     setupSubscriptionForm();
     displaySubscribedFeeds();
     setupBackButton();
   }
+
+  if (document.querySelector('#settings-button')) {
+    document.getElementById('settings-button').addEventListener('click', () => {
+      window.location.href = 'settings.html';
+    });
+  }
 });
 
-// ...
+
+
+function getSubscribedFeeds() {
+  return JSON.parse(localStorage.getItem(SUBSCRIBED_FEEDS_KEY)) || [DEFAULT_FEED_URL];
+}
+
+function setSubscribedFeeds(feeds) {
+  localStorage.setItem(SUBSCRIBED_FEEDS_KEY, JSON.stringify(feeds));
+}
+
+async function loadSubscribedFeeds() {
+  const feeds = getSubscribedFeeds();
+  let allItems = [];
+
+  for (const feedURL of feeds) {
+    const feed = loadFeed(feedURL);
+    allItems = allItems.concat(feed.items);
+  }
+
+  allItems.sort(compareItemsByDate);
+
+  const feedContainer = document.getElementById('feed-container');
+  allItems.forEach(item => {
+    const card = createCard(item);
+    feedContainer.appendChild(card);
+  });
+}
+
+function compareItemsByDate(a, b) {
+  const dateA = new Date(a.pubDate);
+  const dateB = new Date(b.pubDate);
+
+  if (dateA > dateB) {
+    return -1;
+  } else if (dateA < dateB) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+
+async function loadFeed(feedURL) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(
+      { action: 'fetchFeed', feedURL },
+      (response) => {
+        if (response.success) {
+          resolve(response.feed);
+        } else {
+          reject(response.error);
+        }
+      }
+    );
+  });
+}
+
+async function loadSubscribedFeeds() {
+  const feeds = getSubscribedFeeds();
+  const allItems = await Promise.all(feeds.map(loadFeed))
+    .then((feeds) => {
+      return feeds.flatMap(feed => feed.items);
+    })
+    .then((items) => {
+      items.sort(compareItemsByDate);
+      return items;
+    });
+
+  const feedContainer = document.getElementById('feed-container');
+  allItems.forEach(item => {
+    const card = createCard(item);
+    feedContainer.appendChild(card);
+  });
+}
+
+
+function setupSubscriptionForm() {
+  const form = document.getElementById('subscription-form');
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const feedURL = form.elements['feed-url'].value;
+    const feeds = getSubscribedFeeds();
+    feeds.push(feedURL);
+    setSubscribedFeeds(feeds);
+
+    await loadFeed(feedURL);
+    form.reset();
+  });
+}
 
 function displaySubscribedFeeds() {
   const feeds = getSubscribedFeeds();
@@ -48,48 +141,6 @@ function setupBackButton() {
   const backButton = document.getElementById('back-to-main');
   backButton.addEventListener('click', () => {
     window.location.href = 'index.html';
-  });
-}
-
-function getSubscribedFeeds() {
-  return JSON.parse(localStorage.getItem(SUBSCRIBED_FEEDS_KEY)) || [DEFAULT_FEED_URL];
-}
-
-function setSubscribedFeeds(feeds) {
-  localStorage.setItem(SUBSCRIBED_FEEDS_KEY, JSON.stringify(feeds));
-}
-
-async function loadSubscribedFeeds() {
-  const feeds = getSubscribedFeeds();
-  for (const feedURL of feeds) {
-    await loadFeed(feedURL);
-  }
-}
-
-async function loadFeed(feedURL) {
-  const parser = new RSSParser();
-  const feed = await parser.parseURL(feedURL);
-
-  const feedContainer = document.getElementById('feed-container');
-
-  feed.items.forEach(item => {
-    const card = createCard(item);
-    feedContainer.appendChild(card);
-  });
-}
-
-function setupSubscriptionForm() {
-  const form = document.getElementById('subscription-form');
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const feedURL = form.elements['feed-url'].value;
-    const feeds = getSubscribedFeeds();
-    feeds.push(feedURL);
-    setSubscribedFeeds(feeds);
-
-    await loadFeed(feedURL);
-    form.reset();
   });
 }
 
@@ -119,6 +170,15 @@ function createCard(item) {
   }
 
   if (item.link) {
+    const link = new URL(item.link);
+    
+    const date = new Date(item.pubDate);
+    const dateString = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+
+    const details = document.createElement('p');
+    details.textContent = `${dateString}`;
+    textContentDiv.appendChild(details);
+
     const readMoreLink = document.createElement('a');
     readMoreLink.href = item.link;
     readMoreLink.target = '_blank';
@@ -131,6 +191,21 @@ function createCard(item) {
 
   return card;
 }
+
+async function getWebsiteTitle(url) {
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, "text/html");
+    const title = doc.querySelector('title');
+    return title ? title.textContent : url;
+  } catch (e) {
+    console.error('Failed to get website title:', e);
+    return url;
+  }
+}
+
 
 function extractThumbnailURL(content) {
   const imgRegex = /<img[^>]+src="([^">]+)"/;
